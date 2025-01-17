313ef9e7d6c520e41b5f7a87b691080510ecd9b9
Andrew Evans <>
2025-01-16 16:35:28 -0800

    Server: Add eslint, prettier.
    This lives in the server `vourer` directory to avoid having any dev-related code outside of the server directory. Example command: `./vourer/node_modules/eslint/bin/eslint.js -c vourer/eslint.config.mjs vourer/eslint.config.mjs`

---
+++ vourer/eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: false,
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
+]
