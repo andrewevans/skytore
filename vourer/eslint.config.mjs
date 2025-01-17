14a5fd108fd2d120fcf0fdbca69230b58d318047
Andrew Evans <>
2025-01-16 19:11:01 -0800

    CS. eslint.

--- vourer/eslint.config.mjs
+++ vourer/eslint.config.mjs
@@ -5,7 +5,7 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {languageOptions: { globals: { ...globals.node, ...globals.browser }}},
   pluginJs.configs.recommended,
   {
     rules: {
