f84d37d9147d0b805ae189ae88631e80fe6d16e5
Andrew Evans <>
2025-01-21 04:37:42 -0800

    App: Cache files.

--- sw.js
+++ sw.js
@@ -5,9 +5,19 @@ self.importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js",
 )
 
-const CACHE = "pwabuilder-page"
+const CACHE_NAME = "pwabuilder-page"
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/skvto.css",
+  "/js/skvto.js",
+  "vourer/favicon_io/android-chrome-512x512.png",
+  "vourer/favicon_io/android-chrome-192x192.png",
+  "vourer/offline.html",
+]
+
 const offlineFallbackPage = "vourer/offline.html"
 
 self.addEventListener("message", (event) => {
@@ -16,15 +26,35 @@ self.addEventListener("message", (event) => {
   }
 })
 
-self.addEventListener("install", async (event) => {
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage)),
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      cache.addAll(APP_STATIC_RESOURCES)
+    })(),
   )
 })
 
 if (self.workbox.navigationPreload.isSupported()) {
   self.workbox.navigationPreload.enable()
 }
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys()
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name)
+          }
+        }),
+      )
+      await self.clients.claim()
+    })(),
+  )
+})
 
 self.addEventListener("fetch", (event) => {
   if (event.request.mode === "navigate") {
@@ -40,7 +70,7 @@ self.addEventListener("fetch", (event) => {
           const networkResp = await fetch(event.request)
           return networkResp
         } catch (error) {
-          const cache = await caches.open(CACHE)
+          const cache = await caches.open(CACHE_NAME)
           const cachedResp = await cache.match(offlineFallbackPage)
           return cachedResp
         }
